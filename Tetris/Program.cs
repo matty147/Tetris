ea3c2b88e8b1eb091a37e9802de8d3acfada125f
Matty Hana <matty.hana@gmail.com>
2023-05-16 15:39:03 +0200

    wip

--- Tetris/Program.cs
+++ Tetris/Program.cs
@@ -72,16 +72,39 @@ namespace Tetris
 			int blockX = 4;
 			int blockY = 0;
 			board.Data[blockX, blockY] = 1;
-			board.Data[4,15] = 1;
+			board.Data[4,15] = 0;
 			board.Print();
+			int moveXAxis = 0;
+			Console.TreatControlCAsInput = true;
+			ConsoleKeyInfo cki;
+			MyConsole.Color("Type S to start" , ConsoleColor.Green);
+			cki = Console.ReadKey();
+			int keyEnterd = 1;
 			for (; ; )
 			{
+				if (keyEnterd == 0)
+				{
+					cki = Console.ReadKey();
+					keyEnterd = 1;
+					if (cki.ToString() == "A") //this dosen't work :/
+					{
+						Console.WriteLine("Here");
+						moveXAxis = -1;
+					}else if (cki.ToString() == "D")
+					{
+						moveXAxis = 1;
+					}
+				}
+				Console.WriteLine(cki.Key.ToString());
 				if (blockShoudMove < 0)
 				{
 					Console.Clear();
-					if (blockY <= 19 && board.Data[blockX, blockY + 1] == 0)
+					if (blockY <= 18 && board.Data[blockX, blockY + 1] == 0)
 					{
 						board.Data[blockX, blockY] = 0;
+						blockX = blockX + moveXAxis;
+						Console.WriteLine(blockX);
+						Console.WriteLine(moveXAxis);
 						blockY++;
 						board.Data[blockX, blockY] = 1;
 					}
@@ -92,11 +115,13 @@ namespace Tetris
 						board.Data[blockX, blockY] = 1;
 					}
 					board.Print();
+					moveXAxis = 0;
 					blockShoudMove = blockShoudMoveDef;
 					if (blockShoudMoveDef > 5) // makes the game faster
 					{
 						blockShoudMoveDef = blockShoudMoveDef - 0.01f;
 					}
+					keyEnterd = 0;
 				}
 				else
 				{
@@ -104,6 +129,7 @@ namespace Tetris
 					Thread.Sleep(50);
 					blockShoudMove--;
 				}
+				//Console.ReadKey(true);
 			}
 			Console.ReadKey();
 		}
